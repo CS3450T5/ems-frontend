7b4b138921738dabe819f0f87ec4d9be2ee2950e
Peter Shakespear <peter.shakespear@gmail.com>
2025-03-04 21:13:53 -0700

    bloat gone and pages added (see side menu)

---
+++ src/dashboard/components/Settings.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Box, Stack } from '@mui/material';
+import { alpha } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppNavbar from './AppNavbar';
+import Header from './Header';
+import SideMenu from './SideMenu';
+import AppTheme from '../../shared-theme/AppTheme';
+import CustomizedDataGrid from './CustomizedDataGrid';
+import {
+  chartsCustomizations,
+  dataGridCustomizations,
+  datePickersCustomizations,
+  treeViewCustomizations,
+} from '../theme/customizations';
+
+const xThemeComponents = {
+  ...chartsCustomizations,
+  ...dataGridCustomizations,
+  ...datePickersCustomizations,
+  ...treeViewCustomizations,
+};
+
+export default function Settings(props: { disableCustomTheme?: boolean }) {
+  return (
+    <AppTheme {...props} themeComponents={xThemeComponents}>
+      <CssBaseline enableColorScheme />
+      <Box sx={{ display: 'flex' }}>
+        <SideMenu />
+        <AppNavbar />
+        {/* Main content */}
+        <Box
+          component="main"
+          sx={(theme) => ({
+            flexGrow: 1,
+            backgroundColor: alpha(theme.palette.background.default, 1),
+            overflow: 'auto',
+          })}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              alignItems: 'center',
+              mx: 3,
+              pb: 5,
+              mt: { xs: 8, md: 0 },
+            }}
+          >
+            <Header />
+            <p>Settings page</p>
+          </Stack>
+        </Box>
+      </Box>
+    </AppTheme>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dashboard from './dashboard/Dashboard'
+import App from './App'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dashboard />
+    <App />
   </StrictMode>,
 )
