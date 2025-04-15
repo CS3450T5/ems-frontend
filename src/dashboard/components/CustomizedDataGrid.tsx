2158e9c1179148644e65e83396d15813d4f6ec07
brewkirk <brackennewkirk@gmail.com>
2025-04-14 21:09:49 -0600

    Centralized Device page, minor API functionality

--- src/dashboard/components/CustomizedDataGrid.tsx
+++ src/dashboard/components/CustomizedDataGrid.tsx
@@ -1,7 +1,73 @@
 import { DataGrid } from '@mui/x-data-grid';
-import { columns, rows } from '../internals/data/gridData';
+import { useState, useEffect } from 'react';
+import { fetchData } from '../../api.ts';
+import { GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  { field: 'device', headerName: 'Device Name', flex: 1.5, minWidth: 200 },
+  {
+    field: 'voltage',
+    headerName: 'Voltage',
+    headerAlign: 'right',
+    align: 'right',
+    flex: 1,
+    minWidth: 80,
+  },
+  {
+    field: 'current',
+    headerName: 'Current',
+    headerAlign: 'right',
+    align: 'right',
+    flex: 1,
+    minWidth: 100,
+  },
+  {
+    field: 'power',
+    headerName: 'Power',
+    headerAlign: 'right',
+    align: 'right',
+    flex: 1,
+    minWidth: 120,
+  },
+  {
+    field: 'state',
+    headerName: 'State',
+    headerAlign: 'right',
+    align: 'right',
+    flex: 1,
+    minWidth: 100,
+  }
+];
+
+//const rows: GridRowsProp = [
+  //{
+    //id: 1,
+    //device: 'bruh',
+    //voltage: 1,
+    //current: 12,
+    //power: 7,
+    //state: 'Disconnected'
+  //}
+//];
 
 export default function CustomizedDataGrid() {
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const maxEnergy = await fetchData('/max-energy');
+        console.log('Fetched data:', maxEnergy); 
+        setRows(maxEnergy);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchDataFromAPI();
+  }, []);
+
   return (
     <DataGrid
       checkboxSelection
